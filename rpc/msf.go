07e023e1abd7eaf22675fee594efd57eeda31aa1
aokimio <fullunull@gmail.com>
2022-10-10 12:12:07 +0800

    debug info

--- rpc/msf.go
+++ rpc/msf.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
@@ -36,8 +38,8 @@ func (msf *Metasploit) send(req interface{}, res interface{}) error {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	dest := fmt.Sprintf("%s/api", msf.host)
 	response, err := http.Post(dest, "binary/message-pack", buf)
-	// responseBytes, _ := httputil.DumpResponse(response, true)
-	// log.Printf("Response dump: %s\n", string(responseBytes))
+	responseBytes, _ := httputil.DumpResponse(response, true)
+	log.Printf("Response dump: %s\n", string(responseBytes))
 	if err != nil {
 		return err
 	}
